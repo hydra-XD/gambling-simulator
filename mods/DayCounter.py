a7fbecccd8ee19ab06c9af68891252eba0ee7f61
Chloe Boyer <124934873+chloe-gpt@users.noreply.github.com>
2025-01-19 13:55:22 +0000

    Reuploaded saved mods for testing

---
+++ mods/DayCounter.py
@@ -0,0 +1,23 @@
+class DayCounter:
+    def dayStart(self, globals):
+        local_globals = globals
+
+        btn = local_globals["btn"]
+        clear_screen = local_globals["clear_screen"]
+
+        clear_screen()
+
+        print("Day:", local_globals["days"])
+
+        input(f"\nPress {btn('enter')} to continue\n")
+
+        return local_globals
+
+    def __str__(self):
+        return "DayCounter"
+
+    def name(self, globals):
+        return "DayCounter"
+
+    def description(self):
+        return "Adds a day counter at the beginning of each day"
---
+++ mods/Gay.py
@@ -0,0 +1,23 @@
+class Gay:
+    def onRun(self, globals):
+        local_globals = globals
+
+        btn = local_globals["btn"]
+        clear_screen = local_globals["clear_screen"]
+
+        local_globals["spouse"] = "husband"
+
+        return local_globals
+    
+    def __str__(self):
+        return "GayMod"
+        
+    def description(self):
+        return "Makes you gay"
+
+    def name(self, globals):
+        local_globals = globals
+        Fore = local_globals["Fore"]
+        Style = local_globals["Style"]
+
+        return f"{Fore.RED}G{Fore.YELLOW}a{Fore.GREEN}y{Fore.BLUE}M{Fore.MAGENTA}o{Fore.MAGENTA}{Style.DIM}d{Style.RESET_ALL}"
